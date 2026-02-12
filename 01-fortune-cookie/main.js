81a3d2a5e36d6364c7c338ebf3cec4041bbae90c
RaikoMoons <raiko.moons@student.ehb.be>
2026-02-12 10:47:19 +0100

    oefening 1 gedaan

--- 01-fortune-cookie/main.js
+++ 01-fortune-cookie/main.js
@@ -1,16 +1,16 @@
 const fortunes = [
-    "A fresh start will put you on your way.",
-    "Breathe in, breathe out, code.",
-    "Your creativity will lead you to success.",
-    "Trust the process; the logic will click into place soon.",
-    "You will conquer the infinite loop before it crashes your browser.",
-    "Today is the day you actually understand how Git works.",
-    "The error message is not judging you; it is trying to help.",
-    "You love peace, harmony and ChatGPT",
-    "Change is happening in your life, so commit often!",
-    "You will inherit a large sum of money and an undocumented codebase",
-    "Your hard work will pay off soon(-ish).",
-    "The best is yet to come. A semester of Javascript awaits you."
+	"A fresh start will put you on your way.",
+	"Breathe in, breathe out, code.",
+	"Your creativity will lead you to success.",
+	"Trust the process; the logic will click into place soon.",
+	"You will conquer the infinite loop before it crashes your browser.",
+	"Today is the day you actually understand how Git works.",
+	"The error message is not judging you; it is trying to help.",
+	"You love peace, harmony and ChatGPT",
+	"Change is happening in your life, so commit often!",
+	"You will inherit a large sum of money and an undocumented codebase",
+	"Your hard work will pay off soon(-ish).",
+	"The best is yet to come. A semester of Javascript awaits you.",
 ];
 
 // 1. Select Elements
@@ -18,7 +18,11 @@ let display = document.querySelector("#fortune-display");
 let button = document.querySelector("#cookie-btn");
 
 // 2. Add event Listener
-//      generate a random index
-//      get the fortune string
-//      update the display
-
+button.addEventListener("click", function () {
+	//      generate a random index
+	let randomIndex = Math.floor(Math.random() * fortunes.length);
+	//      get the fortune string
+	let fortune = fortunes[randomIndex];
+	//      update the display
+	display.textContent = fortune;
+});
